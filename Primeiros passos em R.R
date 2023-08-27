2c79abcd2a37eaf82915be9dbda6355254e797d5
FlavioAtaliba <flavioatalibajr@gmail.com>
2023-08-26 21:12:07 -0300

    Primeiro passos em R

---
+++ Primeiros passos em R.R
@@ -0,0 +1,27 @@
+print('Olá mundo!')
+# visualizar a string Ola mundo! no console
+a = 2
+# variavel implicita integer alocando o valor 2
+print(a)
+# visualisar o valor contido na variavel "a" no console
+b = 3
+print(b)
+c= 4
+print(c)
+d<-6
+# "<-" outro modo de atribuir valor a variável
+print(d)
+# nome de variavel nao pode começar com número,nem espaço,Case sensitve
+# ctrl s save automativo
+
+
+
+
+
+
+
+
+
+
+
+
